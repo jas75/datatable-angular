1c2ae8df6df3162f6f2091c3124d0210e2f20bbf
Jason <azoulayjason@gmail.com>
2022-09-26 17:26:29 +0200

    create data table component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'data-table',
+    component: DataTableComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
