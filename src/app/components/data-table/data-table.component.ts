769c771011540f5dca273ed999a750df7dfd4b4d
jas <azoulayjason@gmail.com>
2022-10-07 14:57:55 +0200

    insert data in data source

--- src/app/components/data-table/data-table.component.ts
+++ src/app/components/data-table/data-table.component.ts
@@ -14,7 +14,8 @@ import { saveAs } from 'file-saver';
 })
 export class DataTableComponent implements AfterViewInit, OnInit {
   displayedColumns: string[] = ['id', 'name', 'firstname', 'mail', 'action'];
-  dataSource!: MatTableDataSource<Guest>;
+  // dataSource!: MatTableDataSource<Guest>;
+  dataSource = new MatTableDataSource<Guest>(GUEST_DATA);
 
   public positionToEdit!: number;
   @ViewChild(MatPaginator)
@@ -33,7 +34,7 @@ export class DataTableComponent implements AfterViewInit, OnInit {
   }
 
   ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
@@ -59,11 +60,11 @@ export class DataTableComponent implements AfterViewInit, OnInit {
     })
   }
 
-  public onRemoveBtnClick(element: PeriodicElement) {
+  public onRemoveBtnClick(element: Guest) {
     let confirmation = confirm(`Sure you want to delete ${element.name}?`)
 
     if (confirmation) {
-      ELEMENT_DATA = ELEMENT_DATA.filter((x: PeriodicElement) => x.position !== element.position);
+      GUEST_DATA = GUEST_DATA.filter((x: Guest) => x.id !== element.id);
       
       this.dataSource.data = GUEST_DATA;
       this.createRowForm();
@@ -75,14 +76,14 @@ export class DataTableComponent implements AfterViewInit, OnInit {
     this.positionToEdit = -1;
   }
 
-  public validateRow(element: PeriodicElement) {
-    ELEMENT_DATA = ELEMENT_DATA.map((x: PeriodicElement) => {
-      if (x.position === element.position) {
+  public validateRow(element: Guest) {
+    GUEST_DATA = GUEST_DATA.map((x: Guest) => {
+      if (x.id === element.id) {
         return {
-          position: x.position,
+          id: x.id,
           name: this.rowForm.value.name,
-          weight: this.rowForm.value.weight,
-          symbol: this.rowForm.value.symbol,
+          firstname: this.rowForm.value.firstname,
+          mail: this.rowForm.value.mail,
         }
       }
       return x;
@@ -101,7 +102,7 @@ export class DataTableComponent implements AfterViewInit, OnInit {
   }
 
   public onFileSelect(event: any) {
-    console.log(event)
+    // console.log(event)
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.uploadForm.get('file')?.setValue(file);
@@ -118,10 +119,18 @@ export class DataTableComponent implements AfterViewInit, OnInit {
     if (!this.uploadForm.invalid) {
       let fileReader = new FileReader();
       fileReader.onload = (e) => {
+        console.log('e')
+        console.log(e)
+          console.log('------')
+          console.log('fileReader.result')
+        console.log(fileReader.result)
+        console.log('------')
+        console.log(JSON.parse(this.csvToJson(fileReader.result as string)))
         // console.log(fileReader.result);
         // console.log(JSON.parse(this.csvToJson(fileReader.result as string)))
-        this.dataSource = new MatTableDataSource<Guest>(JSON.parse(this.csvToJson(fileReader.result as string)));
+        // this.dataSource = new MatTableDataSource<Guest>(JSON.parse(this.csvToJson(fileReader.result as string)));
         // console.log(this.csvToJson(fileReader.result as string));
+        this.dataSource.data = JSON.parse(this.csvToJson(fileReader.result as string));
         
         // setTimeout(() => {
         //   this.dataSource.paginator = this.paginator;
@@ -129,7 +138,7 @@ export class DataTableComponent implements AfterViewInit, OnInit {
         // this.dataSource.paginator = this.paginator;
 
       }
-      this.dataSource.paginator = this.paginator;
+      // this.dataSource.paginator = this.paginator;
       fileReader.readAsText(this.uploadForm.get('file')?.value);
       // console.log(this.uploadForm.get('file'));
     } else {
@@ -139,7 +148,7 @@ export class DataTableComponent implements AfterViewInit, OnInit {
 
   private csvToJson(csv: string) {
     var lines=csv.split("\n");
-
+console.log(lines)
     var result = [];
 
     // NOTE: If your columns contain commas in their values, you'll need
@@ -163,36 +172,36 @@ export class DataTableComponent implements AfterViewInit, OnInit {
   }
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// }
 
 
-let ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
+// let ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+//   {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+//   {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+//   {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+//   {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+//   {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+//   {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+//   {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+//   {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+//   {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+//   {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+// ];
 
 
 export interface Guest {
